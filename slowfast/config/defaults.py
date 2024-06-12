1bf49269eb612d8fe9d5122c85b8bdc82f18cb67
obrookes <dl18206@bristol.ac.uk>
2024-06-12 12:39:23 +0100

    return cas in cfg

--- slowfast/config/defaults.py
+++ slowfast/config/defaults.py
@@ -320,6 +320,8 @@ _C.TEST.SAVE_RESULTS_PATH = ""
 _C.TEST.NUM_TEMPORAL_CLIPS = []
 
 _C.TEST.RETURN_FEATS = False
+
+_C.TEST.RETURN_CAS = False
 # -----------------------------------------------------------------------------
 # ResNet options
 # -----------------------------------------------------------------------------
